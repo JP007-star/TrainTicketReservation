ff7e337fbc20646a2b5eb8889e933b7645361db9
JP007-Star <justice_of_peace@yahoo.com>
2021-12-08 18:39:06 +0530

    payment ui isssues fix

--- src/main/java/com/torryharris/model/TrainDAO.java
+++ src/main/java/com/torryharris/model/TrainDAO.java
@@ -15,9 +15,9 @@ public class TrainDAO {
 
     //This function test connection with mysql DB
     public void dataBaseConnection() throws ClassNotFoundException, SQLException {
-        db_URL = "jdbc:mysql://localhost:3306/project";
+        db_URL = "jdbc:mysql://localhost:3306/trainticketreservationsystem";
         username = "root";
-        password = "kart@2021";
+        password = "Prasad@66";
         driverName = "com.mysql.cj.jdbc.Driver";
         Class.forName(driverName);
         connection = DriverManager.getConnection(db_URL, username, password);
